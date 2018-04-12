dcfd61c0316acceb2ce954520c957d8820298adc
Vaše Jméno <vas@email.com>
2018-04-12 15:13:59 +0200

    upravy 5. práce webového programování.

--- index.php
+++ index.php
@@ -8,41 +8,47 @@
     <h1>proměnné</h1>
     <?php
 
-    $vodkaPrice = 119 ;
+    $vodkaPrice = 345 ;
     $homelessMoney = 25 ;
     $studentka = 17 ;
     $Muž = 40 ;
     $Podnikatel = 100 ;
-    $obcani = 50 ;
+    $skupina = 50 ;
 
 
 
 
 
      ?>
-     <?php
+     <p><?php
      echo "Bezdomovec má $homelessMoney kc" ;
-     $homelessMoney = $homelessMoney + $studentka ;
-     echo "Soucítící studentka, čekající na vlak, vkládá do čepice 17 kč. Bezdomovec má $homelessMoney" ;
-     $homelessMoney = $homelessMoney + $Muž ;
-     echo "Muž, který právě skončil úspěšně s přijímacím pracovním pohovorem, věnuje bezdomovci peníze v hodnotě 40 Kč. Bezdomovec má $homelessMoney" ;
-     $homelessMoney = $homelessMoney + $Podnikatel ;
-     echo "Před vstupem do pendolina jde kolem bezdomovce bohatý podnikatel. Aby neměl těžkou peněženku zbaví se drobné bankovky v hodnotě 100 Kč.Bezdomovec má $homelessMoney" ;
-     $homelessMoney = $homelessMoney - $skupina ;
-     echo "Kolem čepice bezdomovce se nenápadně prochází skupinka nepřizpůsobivých spoluobčanů a sebere mu z ní 50 Kč. Bezdomovec má $homelessMoney" ;
+     $homelessMoney = $homelessMoney + $studentka ; ?></p>
+    <p> <?php  echo "Soucítící studentka, čekající na vlak, vkládá do čepice 17 kč. Bezdomovec má $homelessMoney" ;
+     $homelessMoney = $homelessMoney + $Muž ; ?> </p>
+  <p> <?php  echo "Muž, který právě skončil úspěšně s přijímacím pracovním pohovorem, věnuje bezdomovci peníze v hodnotě 40 Kč. Bezdomovec má $homelessMoney" ;
+     $homelessMoney = $homelessMoney + $Podnikatel ; ?> </p>
+    <p> <?php   echo "Před vstupem do pendolina jde kolem bezdomovce bohatý podnikatel. Aby neměl těžkou peněženku zbaví se drobné bankovky v hodnotě 100 Kč.Bezdomovec má $homelessMoney" ;
+     $homelessMoney = $homelessMoney - $skupina ; ?> </p>
+    <p> <?php echo "Kolem čepice bezdomovce se nenápadně prochází skupinka nepřizpůsobivých spoluobčanů a sebere mu z ní 50 Kč. Bezdomovec má $homelessMoney" ; ?> </p>
 
 
-     echo "Peníze Bezdomovce po navštěvě večerky:  $homelessMoney. "
+    <p> <?php echo "Peníze Bezdomovce před návštěvou večerky:  $homelessMoney. " ?> </p>
 
-     ?>
+
+
 
      <?php if ($homelessMoney >= $vodkaPrice)
-     { $homelessMoney = $homelessMoney - $vodkaPrice ;
-       echo "bezdomovec má vodku";
-       
+     {
+       $homelessMoney = $homelessMoney - $vodkaPrice ; ?>
+      <p> <?php echo "bezdomovec má vodku"; ?> </p>
+    <?php  } else { ?> <p> <?php echo "bezdomovec nemá peníze";
 
-       # code...
-     } ?>
+         # code...
+       } ?>
+        <p> <?php  echo "Peníze Bezdomovce po návštěvě večerky:  $homelessMoney"; ?> </p>
+
+
+
 
 
 
